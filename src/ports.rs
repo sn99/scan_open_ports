233240c8f5daa714124b3982e4312b0fb27fd1f0
sn99 <siddharthn.099@gmail.com>
2022-06-22 00:27:18 +0530

    works

---
+++ src/ports.rs
@@ -0,0 +1,37 @@
+use crate::{
+    common_ports::MOST_COMMON_PORTS_100,
+    model::{Port, Subdomain},
+};
+use rayon::prelude::*;
+use std::net::{SocketAddr, ToSocketAddrs};
+use std::{net::TcpStream, time::Duration};
+
+pub fn scan_ports(mut subdomain: Subdomain) -> Subdomain {
+    subdomain.open_ports = MOST_COMMON_PORTS_100
+        .into_iter()
+        .map(|port| scan_port(&subdomain.domain, *port))
+        .filter(|port| port.is_open)
+        .collect();
+
+    subdomain
+}
+
+fn scan_port(hostname: &str, port: u16) -> Port {
+    let timeout = Duration::from_secs(3);
+    let socket_addresses: Vec<SocketAddr> = format!("{}:{}", hostname, port)
+        .to_socket_addrs()
+        .expect("port scanner: Creating socket address")
+        .collect();
+    if socket_addresses.len() == 0 {
+        return Port {
+            port,
+            is_open: false,
+        };
+    }
+    let is_open = if let Ok(_) = TcpStream::connect_timeout(&socket_addresses[0], timeout) {
+        true
+    } else {
+        false
+    };
+    Port { port, is_open }
+}
